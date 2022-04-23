19ba8e027ae49b00580a88e777db17e79487b2a7
Jakub <jakub.xiz1.gorecki@gmail.com>
2022-04-23 14:27:36 +0100

    Delete trash test code from main.cpp

--- Calculator/source/main.cpp
+++ Calculator/source/main.cpp
@@ -1,17 +1,5 @@
 #include <standard_calculator.h>
 
-bool check_input()
-{
-	if (cin.fail())
-	{
-		cin.clear();
-		cin.ignore(std::numeric_limits<std::streamsize>::max());
-		cout << "Bad input date. Try again.\n";
-		return false;
-	}
-	return true;
-}
-
 int main()
 {
 	c_standard_calculator o_calculator;
