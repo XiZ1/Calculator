db7e7767a39c33e1d6a5732958279c25921b9834
Jakub <jakub.xiz1.gorecki@gmail.com>
2022-05-11 17:25:03 +0100

    Delete not use line of code.

--- Calculator/source/main.cpp
+++ Calculator/source/main.cpp
@@ -1,12 +1,8 @@
-#include <calculator.h>
 #include <standard_calculator.h>
-#include <advanced_calculator.h>
 
 int main()
 {
-	//c_calculator o_calculator;
 	c_standard_calculator o_standard_calculator;
-	//c_advanced_calculator o_advanced_calculator;
 
 	o_standard_calculator.start_standard_calculator();
 
