2c7fba56114717c46395b82ec31c3e21951aca92
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2022-05-22 11:49:38 +0200

    Add initial not tested version of length unit conversion. Add initial not working and not tested mass and volume conversion

--- Calculator/include/unit_conversion_calculator.h
+++ Calculator/include/unit_conversion_calculator.h
@@ -21,19 +21,31 @@ private:
 	c_calculator cal;
 
 	enum UNITS_NAME {LENGTH, MASS, VOLUME};
-	static constexpr  int tab_length_unit_size{ 5 };
-	static constexpr  int tab_other_unit_size{ 4 };
-	const string tab_length_unit[tab_length_unit_size]{ "mm", "cm", "dm", "m", "km" };
-	const  string tab_mass_unit[tab_other_unit_size]{ "g", "dag", "kg", "t" };
-	const  string tab_volume_mass[tab_other_unit_size]{ "ml", "cl", "dl", "l" };
-	std::vector<string> tab_unit_to_conversion;
+	enum LENGTH_UNIT {mm, cm, dm, m, km};
+	enum MASS_UNIT { g, dag, kg, t };
+	enum VOLUME_UNIT {ml, cl, dl, l};
+
+	float unit = 0;
+
+	static constexpr int tab_length_unit_size{ 5 };
+	static constexpr int tab_other_unit_size{ 4 };
+
+	const string tab_length_units_names[tab_length_unit_size]{ "mm", "cm", "dm", "m", "km" };
+	const string tab_mass_units_names[tab_other_unit_size]{ "g", "dag", "kg", "t" };
+	const string tab_volume_units_names[tab_other_unit_size]{ "ml", "cl", "dl", "l" };
+
+	std::vector<float> result_conversion;
+	std::vector<string> tab_res_con_unit_name;
 
 
 	void choose_conversion_units();
 	void start_length_conversion();
 	void start_mass_conversion();
 	void start_volume_conversion();
-	void choose_unit(const string [], int);
-	void create_unit_tab(const string [], int, int);
+	int choose_unit();
+	void create_unit_name_tab(const string [], const int&, const int&);
+	void get_unit_value(float&);
+	void unit_length_convert(std::vector<float>&, const int);
+	void show_result_conversion();
 	bool whether_exit();
 };
--- Calculator/source/unit_conversion_calculator.cpp
+++ Calculator/source/unit_conversion_calculator.cpp
@@ -63,40 +63,138 @@ void c_unit_conversion_calculator::start_length_conversion()
 		}
 		cal.clear_screen();
 		cal.display_the_message("Choose which unit you want to conversion:\n0. Millimeter [mm]\n1. Centimeter [cm]\n2. Decimeter [dm]\n3. Meter [m]\n4. Kilometer [km]\n5. EXIT");
-		choose_unit(tab_length_unit, tab_length_unit_size);
-
+		const int utc = choose_unit();
+		create_unit_name_tab(tab_length_units_names, tab_length_unit_size, utc);
+		get_unit_value(unit);
+		unit_length_convert(result_conversion, utc);
+		show_result_conversion();
 	}while (end_loop);
 }
 
 void c_unit_conversion_calculator::start_mass_conversion()
 {
+	bool end_loop = true;
+	do
+	{
+		if (!whether_exit())
+		{
+			end_loop = false;
+			continue;
+		}
+		cal.clear_screen();
+		cal.display_the_message("Choose which unit you want to conversion:\n0. Millimeter [mm]\n1. Centimeter [cm]\n2. Decimeter [dm]\n3. Meter [m]\n4. Kilometer [km]\n5. EXIT");
+		const int utc = choose_unit();
+		create_unit_name_tab(tab_mass_units_names, tab_other_unit_size, utc);
+		get_unit_value(unit);
+		//TODO: CREATE CONVERSION FUNCTION
+		show_result_conversion();
+	} while (end_loop);
 }
 
 void c_unit_conversion_calculator::start_volume_conversion()
 {
-}
-
-void c_unit_conversion_calculator::choose_unit(const string tab[], int tab_size)
-{
-	int temp = _getch();
-	for (auto i = 0; i < tab_size; i++)
+	bool end_loop = true;
+	do
 	{
-		if (temp == i)
+		if (!whether_exit())
 		{
-			create_unit_tab(tab, tab_size, temp);
+			end_loop = false;
+			continue;
 		}
-	}
+		cal.clear_screen();
+		cal.display_the_message("Choose which unit you want to conversion:\n0. Millimeter [mm]\n1. Centimeter [cm]\n2. Decimeter [dm]\n3. Meter [m]\n4. Kilometer [km]\n5. EXIT");
+		const int utc = choose_unit();
+		create_unit_name_tab(tab_volume_units_names, tab_other_unit_size, utc);
+		get_unit_value(unit);
+		//TODO: CREATE CONVERSION FUNCTION
+		show_result_conversion();
+	} while (end_loop);
 }
 
-void c_unit_conversion_calculator::create_unit_tab(const string tab[], int tab_size, int delete_unit)
+int c_unit_conversion_calculator::choose_unit()
 {
-	tab_unit_to_conversion.clear();
+	const int utc = _getch();
+	return utc;
+}
+
+void c_unit_conversion_calculator::create_unit_name_tab(const string tab[], const int& tab_size, const int& delete_unit)
+{
+	tab_res_con_unit_name.clear();
 	for (auto i = 0; i < tab_size; i++)
 	{
 		if(i != delete_unit)
 		{
-			tab_unit_to_conversion.push_back(tab[i]);
+			tab_res_con_unit_name.push_back(tab[i]);
 		}
+	}
+}
+
+void c_unit_conversion_calculator::get_unit_value(float& u)
+{
+	cal.clear_screen();
+	cal.display_the_message("Enter value to conversion: ");
+	cin >> u;
+}
+
+void c_unit_conversion_calculator::unit_length_convert(std::vector<float>& res_con, const int unit_length_to_conversion)
+{
+	result_conversion.clear();
+	switch (unit_length_to_conversion)
+	{
+		case mm:
+		{
+			res_con.push_back(unit / 10);
+			res_con.push_back(unit / 100);
+			res_con.push_back(unit / 1000);
+			res_con.push_back(unit / 1000000);
+		}break;
+
+		case cm:
+		{
+			res_con.push_back(unit * 10);
+			res_con.push_back(unit / 10);
+			res_con.push_back(unit / 100);
+			res_con.push_back(unit / 100000);
+		}break;
+
+		case dm:
+		{
+			res_con.push_back(unit * 100);
+			res_con.push_back(unit * 10);
+			res_con.push_back(unit / 10);
+			res_con.push_back(unit / 10000);
+		}break;
+
+		case m:
+		{
+			res_con.push_back(unit * 1000);
+			res_con.push_back(unit * 100);
+			res_con.push_back(unit * 10);
+			res_con.push_back(unit / 1000);
+		}break;
+
+		case km:
+		{
+			res_con.push_back(unit * 1000000);
+			res_con.push_back(unit * 100000);
+			res_con.push_back(unit * 10000);
+			res_con.push_back(unit * 1000);
+		}break;
+
+		default:
+		{
+			NULL;
+		}break;
+	}
+}
+
+void c_unit_conversion_calculator::show_result_conversion()
+{
+	cal.clear_screen();
+	cal.display_the_message("RESULT CONVERSION:\n");
+	for (auto i = 0; i < result_conversion.size(); i++)
+	{
+		cout << result_conversion[i] << " " << tab_res_con_unit_name[i] << "\n";
 	}
 }
 
