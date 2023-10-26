8ad48f3eede2bd3dd682b3297fdb8edf5b27d804
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-10-26 19:45:39 +0200

    Created basic application work flow.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "calculator.h"
+#include "app_ui.h"
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello World!\n";
+	const double f_number = 0.0;
+	const double s_number = 0.0;
+	const char sign = '+';
+
+
+	if (app_ui::checking_input_value(f_number, sign, s_number))
+	{
+		std::cout << "Result calculate " << f_number << " " << sign << " " << s_number << " is: " << calculator::calculate(f_number, sign, s_number);
+	}
+	else
+	{
+		std::cout << "Wrong mathematical char or division by zero.\n";
+	}
 
 	return 0;
 }
