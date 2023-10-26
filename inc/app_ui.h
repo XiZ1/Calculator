04eaba02e13709d257d54ab8e92dd964e90773a1
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-10-26 19:44:10 +0200

    Created application user interface class. Added checking_input_value() method to app_ui class.

---
+++ inc/app_ui.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class app_ui
+{
+	public:
+		static bool checking_input_value(const double& f_number, const char& sign, const double& s_number);
+};
---
+++ src/app_ui.cpp
@@ -0,0 +1,17 @@
+#include "app_ui.h"
+
+bool app_ui::checking_input_value(const double& f_number, const char& sign, const double& s_number)
+{
+	if (sign == '+' || sign == '-' || sign == '*' || sign == '%')
+	{
+		return true;
+	}
+	if (sign == '/')
+	{
+		if (s_number != 0.0)
+		{
+			return true;
+		}
+	}
+	return false;
+}
