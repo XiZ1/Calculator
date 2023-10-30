4e69aeb688c6ad4012202ab1fee71cb4fe9bd951
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-10-30 21:17:02 +0100

    Created calculator_app class.

---
+++ inc/calculator_app.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class calculator_app
+{
+public:
+	void start_app();
+private:
+	double result_ = 0.0, f_number_ = 0.0, s_number_ = 0.0;
+	char sign_ = '+';
+};
---
+++ src/calculator_app.cpp
@@ -0,0 +1,13 @@
+#include "calculator_app.h"
+#include "input.h"
+#include "valid.h"
+#include "calculate.h"
+
+void calculator_app::start_app()
+{
+	input::start_entering_data(&f_number_, &sign_, &s_number_);
+	if (validate::start_validate_input_data(f_number_, sign_, s_number_))
+	{
+		result_ = calculate::start_calculate(f_number_, sign_, s_number_);
+	}
+}
