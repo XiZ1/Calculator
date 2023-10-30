1cd7f0c5670a5d40dc195c4e5b5256b676fd761f
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-10-30 20:47:18 +0100

    Created validate class.

---
+++ src/valid.cpp
@@ -0,0 +1,36 @@
+#include "valid.h"
+
+bool validate::start_validate_input_data(const double& f_number, const char& sign, const double& s_number)
+{
+	if (validate_char(sign))
+	{
+		if (validate_division_by_zero(sign, s_number))
+		{
+			return true;
+		}
+		return false;
+	}
+	return false;
+}
+
+bool validate::validate_char(const char& sign)
+{
+	if (sign == '+' || sign == '-' || sign == '*' || sign == '/' || sign == '%')
+	{
+		return true;
+	}
+	return false;
+}
+
+bool validate::validate_division_by_zero(const char& sign, const double& s_number)
+{
+	if (sign == '/')
+	{
+		if (s_number != 0.0)
+		{
+			return true;
+		}
+		return false;
+	}
+	return true;
+}
