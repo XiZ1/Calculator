27f1a2e4e0cb8b095f4138c4dfd88985d84c7205
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-11-01 13:50:38 +0100

    Added validate_input().

--- inc/input.h
+++ inc/input.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <iostream>
 
 class input
 {
 public:
 	static void start_entering_data(double* f_number, char* sign, double* s_number);
 private:
+	static bool validate_input(const std::string& error_message);
 	static void entering_number(double* number);
 	static void entering_sign(char* sign);
 };
--- src/input.cpp
+++ src/input.cpp
@@ -4,12 +4,35 @@
 
 void input::start_entering_data(double* f_number, char* sign, double* s_number)
 {
-	std::cout << "Enter the first number: ";
-	entering_number(f_number);
-	std::cout << "Enter the sign: ";
-	entering_sign(sign);
-	std::cout << "Enter the second number: ";
-	entering_number(s_number);
+	do
+	{
+		std::cout << "Enter the first number: ";
+		entering_number(f_number);
+	}while (validate_input("Error: Incorrect input value.\n"));
+
+	do
+	{
+		std::cout << "Enter the mathematical sign: ";
+		entering_sign(sign);
+	}while (validate_input("Error: Incorrect input value.\n"));
+
+	do
+	{
+		std::cout << "Enter the second number: ";
+		entering_number(s_number);
+	}while (validate_input("Error: Incorrect input value.\n"));
+}
+
+bool input::validate_input(const std::string& error_message)
+{
+	if (std::cin.fail())
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << error_message;
+		return true;
+	}
+	return false;
 }
 
 void input::entering_number(double* number)
