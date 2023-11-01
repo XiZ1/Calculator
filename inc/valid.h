cbf351d33e16a57b022b961b9a33946a049e6bd3
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-11-01 12:56:39 +0100

    Rename methode class validate from: start_validate_input_date() to: start_validate_date().

--- inc/valid.h
+++ inc/valid.h
@@ -3,7 +3,7 @@
 class validate
 {
 public:
-	static bool start_validate_input_data(const double& f_number, const char& sign, const double& s_number);
+	static bool start_validate_data(const double& f_number, const char& sign, const double& s_number);
 private:
 	static bool validate_char(const char& sign);
 	static bool validate_division_by_zero(const char& sign, const double& s_number);
